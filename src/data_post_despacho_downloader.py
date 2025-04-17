32d632ce9ff6e3207e678e6e0517a9fc3e3d2ecd
Fernando Cornielle <fernandocornielle@gmail.com>
2025-04-16 21:15:12 -0400

    developed the model request which gave a bad response

--- src/data_post_despacho_downloader.py
+++ src/data_post_despacho_downloader.py
@@ -30,8 +30,8 @@ folder = r"data\raw\post_despacho_data"
 os.makedirs(folder, exist_ok=True)
 
 # Define the date range for data download
-start_date = datetime(2025, 3, 21)
-end_date = datetime(2025, 4, 2)
+start_date = datetime(2025, 4, 2)
+end_date = datetime(2025, 4, 16)
 
 print(f"Downloading data for the range: {start_date.strftime('%Y-%m-%d')} - {end_date.strftime('%Y-%m-%d')}")
 
